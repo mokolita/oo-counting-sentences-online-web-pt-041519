410c3866d3767967db6b8a0f5e3b1e5c83a0ff8e
Meg Galvez <meg@oakmaine.com>
2019-05-06 20:57:38 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    end 
+    false 
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    end 
+    false 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true 
+    end 
+    false 
   end
 
   def count_sentences
-
+    binding.pry
+    if self.end_with?(".") || self.end_with?("?") || self.end_with?("!")
+      self.split.count 
+    else self.end_with?("..") || self.end_with?("??") || self.end_with?("!!")
+      
   end
 end
